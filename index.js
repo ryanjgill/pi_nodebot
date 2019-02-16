e3df27591a77be12d38be5e1da66b40788c0a9ac
Ryan Gill <ryanjgill2@gmail.com>
2019-02-16 15:42:31 -0600

    cloning old nodebot platform

---
+++ index.js
@@ -0,0 +1,164 @@
+const express = require('express')
+  , app = express()
+  , path = require('path')
+  , http = require('http').Server(app)
+  , socketIO = require('socket.io')(http)
+  , Raspi = require('raspi-io')
+  , ip = require('ip')
+  , PORT = 3000
+  , board = new five.Board({
+      io: new Raspi()
+    })
+  
+function emitUserCount(socketIO) {
+  socketIO.sockets.emit('user:count', socketIO.engine.clientsCount)
+  console.log('Total users: ', socketIO.engine.clientsCount)
+}
+
+//#region Express 
+app.use(express.static(path.join(__dirname + '/public')))
+
+// index route
+app.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
+// variable input controller route
+app.get('/controller', function (req, res, next) {
+  res.sendFile(path.join(__dirname + '/public/controller.html'))
+})
+//#endregion 
+
+// board ready event
+board.on('ready', function (err) {
+  if (err) {
+    board.reset()
+    return
+  }
+
+  function checkForZeroUsers(socketIO) {
+    if (socketIO.engine.clientsCount === 0) {
+      stop()
+    }
+  }
+
+  console.log('board connected! Johnny-Five ready to go.')
+
+  // setup motors 
+  var motor1 = new five.Motor({
+    pins: {
+      pwm: 'GPIO13',
+      dir: 'GPIO19'
+    },
+    invertPWM: true
+  })
+  , motor2 = new five.Motor({
+    pins: {
+      pwm: 'GPIO12',
+      dir: 'GPIO18'
+    },
+    invertPWM: true
+  })
+
+  //#region Digital controls
+  function forward(_speed) {
+    var speed = _speed ? _speed : 255
+    
+    // motor 1 is reversed
+    motor1.reverse(speed)
+    motor2.forward(speed)
+  }
+
+  function reverse(_speed) {
+    var speed = _speed ? _speed : 255
+
+    // motor 1 is reversed
+    motor1.forward(speed)
+    motor2.reverse(speed)
+  }
+
+  function spinLeft(_speed) {
+    var speed = _speed ? _speed : 255 * .8
+
+    // motor 1 is reversed
+    motor1.forward(speed)
+    motor2.forward(speed)
+  }
+
+  function spinRight(_speed) {
+    var speed = _speed ? _speed : 255 * .8
+
+    // motor 1 is reversed
+    motor1.reverse(speed)
+    motor2.reverse(speed)
+  }
+
+  function stop() {
+    motor1.stop()
+    motor2.stop()
+  }
+  //#endregion
+
+
+  // SocketIO 
+  socketIO.on('connection', function (socket) {
+    //console.log('New connection!')
+
+    emitUserCount(socketIO)
+
+    socket.on('forward', forward)
+
+    socket.on('reverse', reverse)
+
+    socket.on('spinLeft', spinLeft)
+
+    socket.on('spinRight', spinRight)
+
+    //#region Analog controls
+    socket.on('leftMotor', function (input) {
+      //console.log('leftMotor: ' + input.force)
+
+      // INVERTED DIRECTIONS FOR THIS MOTOR
+      if (input.direction === 'forward') {
+        motor1.reverse(input.force)
+      } else {
+        motor1.forward(input.force)
+      }
+    })
+
+    socket.on('rightMotor', function (input) {
+      //console.log('rightMotor: ' + input.force)
+
+      if (input.direction === 'forward') {
+        motor2.forward(input.force)
+      } else {
+        motor2.reverse(input.force)
+      }
+    })
+
+    socket.on('stop', function (motor) {
+      //console.log('STOP!')
+      if (!motor) {
+        stop()
+      } else if (motor === 'leftMotor') {
+        motor1.stop()
+      } else {
+        motor2.stop()
+      }
+    })
+    //#endregion
+
+    socket.on('disconnect', function() {
+      checkForZeroUsers(socketIO)
+      emitUserCount(socketIO)
+    })
+  })
+
+  // set the app to listen on PORT
+  http.listen(PORT)
+
+  // log the address and port
+  console.log('Up and running on ' + ip.address() + ':' + PORT)
+})
+
+
+
---
+++ public/js/variableTankControls.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+  socket = io()
+
+  let joystickL = nipplejs.create({
+    zone: document.getElementById('left'),
+    mode: 'static',
+    position: { left: '50%', top: '50%' },
+    color: 'green',
+    size: 200,
+    name: 'LEFT'
+  })
+
+  let joystickR = nipplejs.create({
+    zone: document.getElementById('right'),
+    mode: 'static',
+    position: { left: '50%', top: '50%' },
+    color: 'red',
+    size: 200,
+    name: 'RIGHT'
+  })
+
+  joystickL.on('end', function (joystick, data) {
+    socket.emit('stop', 'leftMotor')
+    document.getElementById('leftMotor').innerHTML = 'stop()'
+  })
+
+  joystickR.on('end', function (joystick, data) {
+    socket.emit('stop', 'rightMotor')
+    document.getElementById('rightMotor').innerHTML = 'stop()'
+  })
+
+  joystickL.on('move', _.throttle(function (joystick, data) {
+    if (!data.hasOwnProperty('direction')) { return }
+    let direction = data.direction.y === 'up'
+      ? 'forward'
+      : 'reverse'
+    let force = data.force * 255 / 1.5
+  
+    force = force > 255 ? 255 : force.toFixed(0)
+
+    socket.emit('leftMotor', {direction, force})
+    document.getElementById('leftMotor').innerHTML = `${direction}(${force})`
+  }, 75, { 'trailing': false }))
+
+  joystickR.on('move', _.throttle(function (joystick, data) {
+    if (!data.hasOwnProperty('direction')) { return }
+    let direction = data.direction.y === 'up'
+      ? 'forward'
+      : 'reverse'
+    let force = data.force * 255 / 1.5
+    
+    force = force > 255 ? 255 : force.toFixed(0)
+    socket.emit('rightMotor', {direction, force})
+    document.getElementById('rightMotor').innerHTML = `${direction}(${force})`
+  }, 75, { 'trailing': false }))
+}
